43258c503fa59013cc52b6cd0b92c5a6445e2823
Dhruvin Vekariya <93385841+djv03@users.noreply.github.com>
2023-01-19 21:27:18 +0530

    Delete Header.js

--- Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Header(props){
-    console.log(props)//this line is very interesting 
-    //it is giving us all the properties which we has passed in the out App.js
-    //so props is nothing but the arguments passed from App to its child components(here Header.js)
-    return <h1> {props.title} there</h1>;
-}
-export default Header
