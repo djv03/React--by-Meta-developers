64201803fdcd657b79678b00f2d544667d879e03
Dhruvin Vekariya <93385841+djv03@users.noreply.github.com>
2023-01-26 23:47:49 +0530

    Add files via upload

---
+++ Aboutus.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Aboutus(){
+    return(
+        <div>
+        <h1> About us</h1>
+        <h2>join our mission</h2>
+       
+    </div>
+    )
+
+}
+
+ export default Aboutus;
